0f690fcaccc31c6f85b718b066b0e15ba111b3b6
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-05-19 11:53:58 +0500

    not completed

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector(".form");
+const alert = document.querySelector(".alert");
+const textInput = document.querySelector("#textInput");
+const submitBtn = document.querySelector("#submitBtn");
+const groceryContainer = document.querySelector(".grocery-container");
+const groceryList = document.querySelector(".grocery-list");
